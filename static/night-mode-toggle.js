7928823b4b1ec8b6d2c494791f76d447d4d44655
Loafaroo <53982793+Loafaroo@users.noreply.github.com>
2023-05-08 23:25:41 -0500

    changed checkmark style, added persistent night mode, added greyed out style on checked list items

--- static/night-mode-toggle.js
+++ static/night-mode-toggle.js
@@ -2,11 +2,22 @@ const toggleSwitch = document.querySelector('#night-mode-toggle');
 const body = document.querySelector('body');
 
 function switchTheme(e) {
-  if (e.target.checked) {
-    body.classList.add('night-mode');
-  } else {
-    body.classList.remove('night-mode');
-  }    
+    if (e.target.checked) {
+        body.classList.add('night-mode');
+        localStorage.setItem('nightMode', 'enabled');
+    } else {
+        body.classList.remove('night-mode');
+        localStorage.setItem('nightMode', null);
+    }
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+const currentTheme = localStorage.getItem('nightMode');
+if (currentTheme === 'enabled') {
+    toggleSwitch.checked = true;
+    body.classList.add('night-mode');
+} else {
+    toggleSwitch.checked = false;
+    body.classList.remove('night-mode');
+}
